13aacc3f70e99bd1d63ce551a35daf308a5fa3d3
marciormta <marcio_mota@usp.br>
2024-09-05 02:43:24 -0300

    inclusao schema

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message
+
+from http import HTTPStatus
+
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "batatinhas fritas"}
